e4a03e3b260e0ff14213534c6ee664e0fd31be7f
hwa-ryong <ocsohy@naver.com>
2023-08-23 16:26:38 +0900

    spring12323

--- stsMvcProj/src/main/java/aaa/model/Person.java
+++ stsMvcProj/src/main/java/aaa/model/Person.java
@@ -3,21 +3,30 @@ package aaa.model;
 import lombok.Data;
 
 @Data
+
 public class Person {
-	
-	String pname, gender;
-	boolean mil;
-	
 	
 	public Person() {
 		// TODO Auto-generated constructor stub
 	}
-	
 	public Person(String pname, String gender, boolean mil) {
 		super();
 		this.pname = pname;
 		this.gender = gender;
 		this.mil = mil;
 	}
+	
+	String pname, gender, id, pw;
+	int age;
+	boolean mil, marriage;
+	public Person(String id, String pname, String pw) {
+		super();
+		this.id = id;
+		this.pname = pname;
+		this.pw = pw;
+	}
+	
+	
+	
 	
 }
